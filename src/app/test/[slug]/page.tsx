3eaf65e081b149db32370e5cbc295f3c27db1c6d
cham0287 <crtmt97@gmail.com>
2023-06-18 23:39:24 +0900

    Feat: 테스트 시작 페이지

--- src/app/test/[slug]/page.tsx
+++ src/app/test/[slug]/page.tsx
@@ -1,3 +1,8 @@
+'use client';
+
+import { useRouter } from 'next/navigation';
+import { MouseEvent } from 'react';
+
 interface TypeTestProps {
   params: {
     slug: string;
@@ -5,7 +10,52 @@ interface TypeTestProps {
 }
 
 const TypeTest = ({ params: { slug } }: TypeTestProps) => {
-  return <div>test name: {slug}</div>;
+  const router = useRouter();
+  const handleClickTestStartBtn = (e: MouseEvent<HTMLButtonElement>) => {
+    router.push('/hi');
+  };
+
+  return (
+    <div id="page_wrap" className="bird_test test_main">
+      <section>
+        <div className="title_area">
+          <div className="sub">내가 새라면 어떤 새일까?</div>
+          <h2 className="title yuji">새 테스트</h2>
+        </div>
+        <div className="img_wrap">
+          <img src="/images/bird/main-01.png" alt="새 테스트 메인 이미지" />
+        </div>
+        <div className="cont_block ">
+          <div className="title">자신이 어떤 새 인지 알아본 사람</div>
+          <div className="content">
+            <div className="test_cnt yuji">99,999,999</div>
+          </div>
+        </div>
+        <div className="cont_block share">
+          <div className="title">친구한테도 알려주기</div>
+          <div className="content">
+            <button className="btn_type_a btn_color_04">
+              <span className="ico_btn ico_btn_url"></span>
+              <span className="btn_txt">URL</span>
+            </button>
+            <button className="btn_type_a btn_color_02">
+              <span className="ico_btn ico_btn_kakao"></span>
+              <span className="btn_txt">카카오</span>
+            </button>
+          </div>
+        </div>
+        <div className="cont_block">
+          <div className="content">
+            <a className="btn_type_b w100 btn_color_01">
+              <button className="btn_txt" onClick={handleClickTestStartBtn}>
+                테스트 시작하기
+              </button>
+            </a>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default TypeTest;
