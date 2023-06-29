321e3d140cfefbab51cb72d795a95e2e6620b4f8
cham0287 <crtmt97@gmail.com>
2023-06-30 00:16:27 +0900

    Feat: 메인페이지 구조변경 및 TestList 컴포넌트 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,26 +1,10 @@
-'use client';
-
-import { useRouter, usePathname } from 'next/navigation';
-import { MouseEvent } from 'react';
+import React from 'react';
+import TestList from '@/components/TestList';
 
 export default function Home() {
-  const router = useRouter();
-  const handleClickTest = (e: MouseEvent<HTMLLIElement>) => {
-    router.push(`/test/${e.currentTarget.id}`);
-  };
-
   return (
     <main className="p-4">
-      <h2 className="text-2xl p-1">테스트 목록</h2>
-      <ul className="p-4">
-        <li
-          id="bird"
-          className="bg-blue-600 hover:bg-blue-500 p-4 rounded-full text-xl text-white cursor-pointer"
-          onClick={handleClickTest}
-        >
-          멸종위기 새 테스트
-        </li>
-      </ul>
+      <TestList />
     </main>
   );
 }
---
+++ src/components/TestList.tsx
@@ -0,0 +1,32 @@
+'use client';
+import { useRouter } from 'next/navigation';
+import React, { MouseEvent } from 'react';
+import { getAllCategories } from '@/api/api';
+import useSWR from 'swr';
+import TestCard from './TestCard';
+import { Spinner } from './common/Spinner';
+
+const TestList = () => {
+  const router = useRouter();
+  const handleClickTest = (e: MouseEvent<HTMLLIElement>) => {
+    router.push(`/test/${e.currentTarget.id}`);
+  };
+  const { data } = useSWR('categories', getAllCategories);
+
+  return (
+    <div>
+      <h2 className="text-2xl p-1">테스트 목록</h2>
+      {data ? (
+        <ul className="p-4 flex justify-center items-center">
+          {data.data.map((testCategory) => {
+            return <TestCard onClick={handleClickTest} key={testCategory.id} data={testCategory} />;
+          })}
+        </ul>
+      ) : (
+        <Spinner />
+      )}
+    </div>
+  );
+};
+
+export default TestList;
