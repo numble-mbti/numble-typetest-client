7b309de5346ad643032c6b93fff537b18bb43230
cham0287 <crtmt97@gmail.com>
2023-07-01 01:14:46 +0900

    Feat: getTestQueries api함수 추가

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,8 +1,17 @@
-import { TestCategoryDTO } from '@/types/types';
-const dest = '/api/categories';
+import { TestCategoryDTO, TestQueriesDTO } from '@/types/types';
 
 export async function getAllCategories(): Promise<TestCategoryDTO> {
-  const uri = `${process.env.NEXT_PUBLIC_API_ENDPOINT}${dest}`;
+  const uri = `${process.env.NEXT_PUBLIC_API_ENDPOINT}/api/categories`;
+  const res = await fetch(uri);
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data');
+  }
+
+  return res.json();
+}
+export async function getTestQueries(categoryId: number): Promise<TestQueriesDTO> {
+  const uri = `${process.env.NEXT_PUBLIC_API_ENDPOINT}/api/mbti/${categoryId}/check`;
   const res = await fetch(uri);
   if (!res.ok) {
     // This will activate the closest `error.js` Error Boundary
