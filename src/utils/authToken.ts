0694d0d5c0f9114af73b2cece044919c0c609c8b
JeongJun <rgfdds98@gmail.com>
2023-07-07 00:10:56 +0900

    !HOTFIX: NextJS 빌드 에러 수정

--- src/utils/authToken.ts
+++ src/utils/authToken.ts
@@ -1,12 +1,16 @@
 export const tokenCookie = {
   setCookie: (name: string, value: string, day: number) => {
-    const date = new Date();
-    date.setTime(date.getTime() + day * 24 * 60 * 60 * 1000);
-    document.cookie = name + '=' + value + ';expires=' + date.toUTCString() + '; path=/;';
+    if (typeof window === 'object') {
+      const date = new Date();
+      date.setTime(date.getTime() + day * 24 * 60 * 60 * 1000);
+      document.cookie = name + '=' + value + ';expires=' + date.toUTCString() + '; path=/;';
+    }
   },
   getCookie: (name: string) => {
-    const value = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
-    return value ? value[2] : null;
+    if (typeof window === 'object') {
+      const value = document.cookie.match('(^|;) ?' + name + '=([^;]*)(;|$)');
+      return value ? value[2] : null;
+    }
   },
   deleteCookie: (name: string) => {
     document.cookie = name + '=; expires=Thu, 01 Jan 1999 00:00:10 GMT; path=/;';
