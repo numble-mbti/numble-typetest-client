61f9640719f5cdf13afc4a6272bb3e0f2e772f3f
JeongJun <rgfdds98@gmail.com>
2023-07-06 21:05:52 +0900

    Feat: axios interceptor 생성 및 내 결과 리스트 불러오기 api 작성

--- src/api/user/api.ts
+++ src/api/user/api.ts
@@ -1,5 +1,8 @@
 import axios from 'axios';
+import { interceptors } from '../interceptor';
 import { tokenCookie } from '@/utils/authToken';
+
+import * as T from '@/types/user';
 
 const BASE_URL = `${process.env.NEXT_PUBLIC_API_ENDPOINT}`;
 
@@ -10,6 +13,7 @@ const HEADERS = {
 };
 
 const api = axios.create({ baseURL: BASE_URL, headers: HEADERS });
+const apiWithToken = interceptors(api);
 
 export async function ssoLogin(social: string): Promise<void> {
   const uri = `/api/oauth2/${social}`;
@@ -31,9 +35,31 @@ export async function ssoLogin(social: string): Promise<void> {
   }
 }
 
-export async function getAccessToken(social: string, code: string) {
+export async function getAccessToken(social: string, code: string): Promise<T.OAuthResponse> {
   const uri = `api/afterlogin/${social}?code=${code}`;
   const res = await api.get(uri);
+
+  const userData = {
+    email: res.data.email,
+    nickname: res.data.nickname,
+  };
+
+  // 유저 데이터 임시로 localStorage에 저장
+  window.localStorage.setItem('userData', JSON.stringify(userData));
   tokenCookie.setCookie('accessToken', res.data.access_token, 1);
   return res.data;
+}
+
+export async function getMyTestResult(size?: number) {
+  const setUri = () => {
+    if (size) {
+      return `api/archives?size=${size}`;
+    } else {
+      return `api/archives?size=3`;
+    }
+  };
+  const uri = setUri();
+  const res = await apiWithToken.get(uri);
+
+  console.log(res);
 }
