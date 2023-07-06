c996eec39149f154b5d2e346d44ba6b0dffff443
cham0287 <crtmt97@gmail.com>
2023-07-06 21:36:28 +0900

    Feat: header바 서비스 로고로 수정 및 미사용 페이지 제거

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -1,10 +1,11 @@
 'use client';
 import React, { useState } from 'react';
-import { BiHome, BiMenu } from 'react-icons/bi';
+import { BiMenu } from 'react-icons/bi';
 import Link from 'next/link';
 import ModalLayout from './ModalLayout';
 
 import { tokenCookie } from '@/utils/authToken';
+import Image from 'next/image';
 
 const Header = () => {
   const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
@@ -22,13 +23,12 @@ const Header = () => {
     <>
       <header className="bg-gray-100 border border-b-gray-300 z-50">
         <div className="gnb">
-          <div className="left_area">
-            <h1 className="text-2xl p-2">흔한 테스트</h1>
+          <div className="left_area" style={{ padding: '10px' }}>
+            <Link href="/">
+              <Image src="/logo.svg" alt="플러피 로고" width={100} height={50} />
+            </Link>
           </div>
           <div className="right_area flex gap-3 items-center px-2 text-2xl">
-            <Link href="/">
-              <BiHome />
-            </Link>
             <BiMenu onClick={() => setIsModalOpen(true)} className="cursor-pointer" />
           </div>
         </div>
