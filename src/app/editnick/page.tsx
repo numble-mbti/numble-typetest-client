acc116727c6f204c4f9ab704b756c4c223b33a37
JeongJun <rgfdds98@gmail.com>
2023-07-07 01:59:12 +0900

    Design: 내 정보 수정하기 페이지 디자인 적용

--- src/app/editnick/page.tsx
+++ src/app/editnick/page.tsx
@@ -23,28 +23,40 @@ const EditMyNickname = () => {
     }
   };
   return (
-    <div id="page_wrap" className="setting edit_info">
-      <section>
-        <h5 className="text-2xl font-bold">닉네임을 입력해주세요</h5>
+    <div id="page_wrap" className="setting edit_info px-8">
+      <section style={{ height: '100vh' }}>
+        <h1 className="text-2xl pt-8 font-bold">닉네임을 입력해주세요</h1>
         <p className="sub">최대 20글자 까지 입력할 수 있습니다.</p>
-        <div className="input_wrap">
-          <input
-            id="nicknameInput"
-            className="px-1 py-2"
-            type="text"
-            value={nickname}
-            onChange={handleInputChange}
-            maxLength={20}
-            required={true}
-            placeholder="닉네임을 입력하세요"
-          />
-          <button onClick={handleClearClick}>Clear</button>
 
-          <div className="char_cnt">
-            <p>{characterCount}/20</p>
+        <div className="edit_form_wrap mt-8">
+          <div className="input_wrap px-3">
+            <div style={{ position: 'relative' }}>
+              <input
+                id="nicknameInput"
+                className="px-1 py-1 w100 h-14 border-b-2 focus:outline-0"
+                type="text"
+                value={nickname}
+                onChange={handleInputChange}
+                maxLength={20}
+                required={true}
+                placeholder="닉네임을 입력하세요"
+              />
+              <button className="clear_btn" onClick={handleClearClick}>
+                <img src="icons/clear_btn.svg" alt="clear 버튼" />
+              </button>
+            </div>
+            <div className="char_cnt">
+              <p>{characterCount}/20</p>
+            </div>
           </div>
+          <button
+            className="px-3 py-4 w100 text-white text-2xl rounded-lg"
+            onClick={handleUpdateClick}
+            style={{ backgroundColor: nickname ? '#0079E3' : '#999999' }}
+          >
+            변경 완료
+          </button>
         </div>
-        <button onClick={handleUpdateClick}>Update Nickname</button>
       </section>
     </div>
   );
--- src/app/myinfo/page.tsx
+++ src/app/myinfo/page.tsx
@@ -4,12 +4,12 @@ import React from 'react';
 
 const EditMyNickname = () => {
   return (
-    <div id="page_wrap" className="setting">
+    <div id="page_wrap" className="setting px-8">
       <h1 className="title text-2xl pt-8 font-bold">내 정보 수정하기</h1>
       <section>
-        <Link href="/editnick" className="w100">
-          <span className="btn_txt">닉네임 수정</span>
-          <span className="ico_btn ico_btn_arr_right_16"></span>
+        <Link href="/editnick" className="p-2 mt-8 flex justify-between">
+          <span className="btn_txt edit_btn">닉네임 수정</span>
+          <span className="ico_btn ico_btn_arr_right_24"></span>
         </Link>
       </section>
     </div>
